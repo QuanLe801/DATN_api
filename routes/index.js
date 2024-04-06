a16f44ca16d51bdb30740ff98502ca68e5d53498
quanlm1 <quanlm1@synodus.com>
2024-04-06 08:26:46 +0700

    updated

--- routes/index.js
+++ routes/index.js
@@ -30,23 +30,52 @@ router.get('/Departments', function (req, result, next) {
 });
 
 router.get('/Assets/GetPaging', function (req, result, next) {
-  var v_name = req.query.name;
+  var v_id = req.query.id;
   var v_typesId = req.query.department;
   var v_departmentsId = req.query.type;
-  console.log(v_name,v_typesId,v_departmentsId);
-  sql.query(` SELECT * FROM assets
-              INNER JOIN departments 
-              ON assets.departmentsId = departments.departmentsId
-              INNER JOIN types ON assets.typesId = types.typesId
-              WHERE name LIKE '${v_name}' OR name LIKE '${v_name}%'
-              AND assets.typesId LIKE CONCAT('%', IFNULL('${v_typesId}', ''), '%')
-              AND assets.departmentsId LIKE CONCAT('%', IFNULL('${v_departmentsId}', ''), '%')
-              ORDER BY assets.code ASC;`
-              , (err, res) => {
-    if (err) throw err;
-    result.json({ res });
-    return ;
-  })
+  console.log(v_id,v_typesId,v_departmentsId);
+  try {
+    sql.query(` SELECT * FROM assets
+                INNER JOIN departments 
+                ON assets.departmentsId = departments.departmentsId
+                INNER JOIN types ON assets.typesId = types.typesId
+                WHERE code = '${v_id}' 
+                AND assets.typesId LIKE CONCAT('%', IFNULL('${v_typesId}', ''), '%')
+                AND assets.departmentsId LIKE CONCAT('%', IFNULL('${v_departmentsId}', ''), '%')
+                ORDER BY assets.code ASC;`, (err, res) => {
+    if (err) {
+      result.status(500).send('Error Query');
+      return;
+    }
+      result.json({ res });
+      return ;
+    })
+  } catch (error) {
+    console.error('Error executing query:', error);
+  }
+});
+
+router.post('/login', function(req, result, next) {
+  const payload = req.body;
+  sql.query(`SELECT * FROM users WHERE username = '${payload.username}' AND password = '${payload.password}';`,
+    (err, res) => {
+      if (err) {
+        // Xử lý lỗi SQL nếu có
+        console.error(err);
+        result.status(500).send('Lỗi máy chủ');
+        return;
+      }
+
+      const userData = res[0]; // Lấy dữ liệu từ hàng đầu tiên trong kết quả
+      console.log(userData);
+
+      if (userData) {
+        result.status(200).json(userData);
+      } else {
+        result.status(404).send('Tên đăng nhập không đúng');
+      }
+    }
+  );
 });
 
 
